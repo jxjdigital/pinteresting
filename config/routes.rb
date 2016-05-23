4ffd7912aa221729b5140b6490f0336e95a6b310
Jessica A. Jennings <jessica.six@gmail.com>
2016-05-23 18:49:57 -0400

    Added dashboard page... maybe!

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Pinteresting::Application.routes.draw do
   root 'pages#home'
   get 'about'=>'pages#about' # creates about_path
-  
+  get 'dashboard'=>'pages#dashboard' # creates dashboard_path
+    
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
